00739504001775c8b67e56da0a34da1f97f2c40b
Hayden Marshall <marshall.hayden65@gmail.com>
2025-04-17 14:45:13 -0500

    added these too

---
+++ resources/js/Components/Layouts/DefaultLayout.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { Button } from 'primevue';
+import { ref } from 'vue';
+
+const searchInput = ref("");
+
+const searchBooks = () => {}
+</script>
+
+<template>
+    <header id="topnavbar" class="border-b border-coffee p-4 flex justify-between items-center bg-tan">
+        <h1 class="text-2xl font-bold ml-4">Cohoes Public Library</h1>
+        <form @submit.prevent="searchBooks">
+            <div class="flex gap-2 items-center border border-coffee p-2 rounded-xl">
+                <div class="flex flex-col">
+                    <input id="search-books" class="bg-black/10 placeholder-black focus:bg-black/50 focus:placeholder-white focus:text-white text-black transition rounded-lg text-sm p-2" placeholder="Search for books..." v-model="searchInput">
+                </div>
+                <Button aria-label="Search for books" icon="pi pi-search" class="text-coffee" iconPos="right"></Button>
+            </div>
+        </form>
+    </header>
+    <main class="bg-almond/75 min-h-[calc(100vh-87px)]">
+        <div class="mx-20 border border-coffee/30">
+            <slot></slot>
+        </div>
+    </main>
+</template>
+
+<style>
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import DefaultLayout from "../Components/Layouts/DefaultLayout.vue";
+
+const fillFeaturedBooks = () => {}
+</script>
+
+<template>
+    <DefaultLayout>
+        <div>
+            Hello
+        </div>
+    </DefaultLayout>
+</template>
+
+<style></style>
