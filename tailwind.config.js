53bc8f6a5745d4cb82343711d8c91597e3d1bc58
Hayden Marshall <marshall.hayden65@gmail.com>
2025-04-17 14:44:53 -0500

    started layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,25 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./resources/views/**/*.blade.php",
+        "./resources/**/*{.vue,.js,.ts,.jsx,.tsx}",
     ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'almond': {
+                    DEFAULT: '#EADDCA'
+                },
+                'tan': {
+                    DEFAULT: '#F5DEB3'
+                },
+                'coffee': {
+                    DEFAULT: '#6F4E37'
+                }
+            }
         },
     },
     plugins: [],
